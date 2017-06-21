99c245b35a784ab3284aefffbe682c829b45546a
John Dias <joaodias@google.com>
2017-06-21 13:14:52 -0700

    sanders: Pre-opt SystemUI
    SystemUI is getting deoptimized, possibly resulting in jank
    (not yet validated); pre-optimize to avoid deoptimization for now.
    Bug: 62832717
    Bug: 35872915
    Test: boot, check for vdex, run for a bit
    
    Change-Id: Ia754db444eebaebf0ff2389ab66f6f1d486da994
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 # Enable dexpreopt to speed boot time
 WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # Display
 BOARD_USES_ADRENO := true
