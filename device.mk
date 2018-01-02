93d1cd262ee9edb2dd9834e14478f1895d93f9a4
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    potter: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -368,6 +368,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
