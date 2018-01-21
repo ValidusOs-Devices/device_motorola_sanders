76c2e2cc4d69784ba3eb4ef359478f5d3ccb9f87
Vachounet <vachounet@live.fr>
2018-01-21 22:12:09 +0100

    Revert "potter: enable WITH_DEXPREOPT"
    * Workaround to fix camera at first boot
    
    (reverted from commit 203ef8abcdf8219949475ebf330122783913fd40)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,6 @@ ENABLE_CPUSETS := true
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Enable dexpreopt to speed boot time
-WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # Display
