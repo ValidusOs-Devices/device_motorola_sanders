a640e60a5e488a0537cd8b198ee080d3acae9278
Bruno Martins <bgcngm@gmail.com>
2018-04-01 10:58:29 +0100

    sanders: libinit: Also override certain vendor build properties

--- init/init_sanders.cpp
+++ init/init_sanders.cpp
@@ -76,12 +76,13 @@ void vendor_load_properties()
     if (platform != ANDROID_TARGET)
         return;
 
+    // sku
+    std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
+    property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
+
     // fingerprint
     property_override("ro.build.description", "sanders-7.1.1/NPS26.116-61/74:user/release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"); // safetynet hax
-
-    std::string sku = GetProperty("ro.boot.hardware.sku", "");
-    property_set("ro.product.model", sku.c_str());
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "motorola/sanders/sanders:7.1.1/NPSS26.116-61-5/12:user/release-keys");
 
     // rmt_storage
     std::string device = GetProperty("ro.boot.device", "");
