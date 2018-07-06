51600f403913b964505d6b41649f85bdc3f050ed
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2018-07-06 13:07:16 +0530

    Set matching build fingerprint for system, vendor and boot images
    * sanders fingerprint got back to the original.

--- init/init_sanders.cpp
+++ init/init_sanders.cpp
@@ -81,8 +81,8 @@ void vendor_load_properties()
     property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
 
     // fingerprint
-    property_override("ro.build.description", "sanders-7.1.1/NPS26.116-61/74:user/release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "motorola/sanders/sanders:7.1.1/NPSS26.116-61-5/12:user/release-keys");
+    property_override("ro.build.description", "sanders-7.1.1/NPS26.116-26/30:user/release-keys");
+    property_set("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"); // safetynet hax
 
     // rmt_storage
     std::string device = GetProperty("ro.boot.device", "");
